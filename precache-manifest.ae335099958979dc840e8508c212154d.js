970f436de37b711dc9bb54efcd2c4da9bb17bd44
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-10-22 23:40:57 +0800

    Updates

---
+++ precache-manifest.ae335099958979dc840e8508c212154d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a67ac0db584545f14efc4b4f720cab",
+    "url": "/qremoji/index.html"
+  },
+  {
+    "revision": "e18abb1813618a0615ba",
+    "url": "/qremoji/static/css/main.30c6f5fa.chunk.css"
+  },
+  {
+    "revision": "42df441e0cbbbd8e4f52",
+    "url": "/qremoji/static/js/2.699faf6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qremoji/static/js/2.699faf6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18abb1813618a0615ba",
+    "url": "/qremoji/static/js/main.770d0972.chunk.js"
+  },
+  {
+    "revision": "63b3b0d759ed25839103",
+    "url": "/qremoji/static/js/runtime-main.5e7497b7.js"
+  },
+  {
+    "revision": "5cf585fb2a38b17195dae89817db5b7f",
+    "url": "/qremoji/static/media/hero-bg.5cf585fb.png"
+  }
+]);
